f61b23b321e982bfeb155a2258103f0959e354e8
andretan <andretan@stud.ntnu.no>
2024-03-15 13:08:01 +0100

    fixed colors of screens

--- core/src/com/mygdx/tank/MultiplayerScreen.java
+++ core/src/com/mygdx/tank/MultiplayerScreen.java
@@ -38,7 +38,7 @@ public class MultiplayerScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.draw();
