d204af7fe902fc768db08cb1d6ae99d2a4c43915
andretan <andretan@stud.ntnu.no>
2024-03-15 13:05:00 +0100

    created a template for the main menu with different options

--- core/src/com/mygdx/tank/TankMazeMayhem.java
+++ core/src/com/mygdx/tank/TankMazeMayhem.java
@@ -1,31 +1,38 @@
 package com.mygdx.tank;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
-public class TankMazeMayhem extends ApplicationAdapter {
+public class TankMazeMayhem extends Game {
 	SpriteBatch batch;
-	Texture img;
-	
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		// Set the initial screen to the main menu
+		setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		super.render(); // Delegates rendering to the current screen
 	}
-	
+
+	public BitmapFont getFont() {
+		// Load and return the font you want to use for buttons
+		return new BitmapFont(); // Example, replace this with your actual font loading code
+	}
+
+	public TextButton.TextButtonStyle getButtonStyle() {
+		TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
+		buttonStyle.font = getFont(); // Ensure to define getFont() method as mentioned earlier
+		return buttonStyle;
+	}
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
 	}
 }
