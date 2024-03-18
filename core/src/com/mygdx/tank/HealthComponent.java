22328a88d16e89116a812f2170dd6bdcae88f90b
IvarTSkaug <ivartobias.skaug@gmail.com>
2024-03-18 22:08:07 +0100

    Basic MVC and ECS, with the tank entity, a few components and a movementSystem. A basic game with the tank moving around.

---
+++ core/src/com/mygdx/tank/HealthComponent.java
@@ -0,0 +1,9 @@
+package com.mygdx.tank;
+
+public class HealthComponent implements Component {
+    public int currentHealth;
+
+    public HealthComponent(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+}
--- core/src/com/mygdx/tank/TankMazeMayhem.java
+++ core/src/com/mygdx/tank/TankMazeMayhem.java
@@ -1,31 +1,27 @@
 package com.mygdx.tank;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
 
 public class TankMazeMayhem extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private GameModel model;
+	private GameView view;
+	private GameController controller;
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		model = new GameModel();
+		view = new GameView(model);
+		controller = new GameController(model);
+
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		controller.update(deltaTime);
+		model.update(deltaTime);
+		view.render();
 	}
 }
+
