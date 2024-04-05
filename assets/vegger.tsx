d0d911b06278cc7c466acb81ef0cc155eaa8b294
vithusas <vithusas@stud.ntnu.no>
2024-04-05 15:00:27 +0200

    map er scaled bra på desktop, ikke på emulator

---
+++ assets/vegger.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="vegger" tilewidth="20" tileheight="20" tilecount="2500" columns="50">
+ <image source="../../../../../OneDrive/Bilder/vegger.jpg" width="1000" height="1000"/>
+</tileset>
---
+++ core/src/com/mygdx/tank/model/GameScreen.java
@@ -0,0 +1,4 @@
+package com.mygdx.tank.model;
+
+public class GameMap {
+}
