9aab3e4936a549fa34b45eacda8bb6fc27b414d7
heidihra <heidihra@stud.ntnu.no>
2024-04-05 12:24:32 +0200

    Remove unused files

--- core/src/com/mygdx/tank/CoreInterfaceClass.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.tank;
-
-public class CoreInterfaceClass implements FireBaseInterface{
-
-    @Override
-    public void SomeFunction() {
-        System.out.println("Hello World!");
-    }
-}
--- core/src/com/mygdx/tank/FireBaseInterface.java
+++
@@ -1,5 +0,0 @@
-package com.mygdx.tank;
-
-public interface FireBaseInterface {
-    public void SomeFunction();
-}
