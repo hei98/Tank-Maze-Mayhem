80d4d10154c6e98eb8b35cca7484d7a0ed199b68
heidihra <heidihra@stud.ntnu.no>
2024-04-08 00:12:58 +0200

    Add background and fix design for main menu

--- core/src/com/mygdx/tank/MainMenuScreen.java
+++ core/src/com/mygdx/tank/MainMenuScreen.java
@@ -3,17 +3,28 @@ package com.mygdx.tank;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import org.w3c.dom.Text;
 
 public class MainMenuScreen implements Screen {
     private final TankMazeMayhem game;
     private Stage stage;
     private SpriteBatch batch;
+    private Texture background;
+    private Skin buttonSkin;
+    private TextButton multiplayerButton, leaderboardButton, loginButton;
+    private ImageButton settingsButton;
 
     public MainMenuScreen(TankMazeMayhem game) {
         this.game = game;
@@ -23,13 +34,18 @@ public class MainMenuScreen implements Screen {
     public void show() {
         batch = new SpriteBatch();
         stage = new Stage();
+        background = new Texture("Backgrounds/main-menu.JPG");
 
-        // Create button style
-        TextButtonStyle buttonStyle = new TextButtonStyle();
-        buttonStyle.font = game.getFont(); // You need to define your font
-        TextButton leaderboardButton = new TextButton("Leaderboard", buttonStyle);
-        TextButton multiplayerButton = new TextButton("Multiplayer", buttonStyle);
-        TextButton settingsButton = new TextButton("Settings", buttonStyle);
+        //Load the skin and atlas for the buttons
+        buttonSkin = new Skin(Gdx.files.internal("skins/orange/skin/uiskin.json"));
+
+        //Create the buttons with the new style
+        multiplayerButton = new TextButton("Multiplayer", buttonSkin, "menu");
+        leaderboardButton = new TextButton("Leaderboard", buttonSkin, "default");
+        loginButton = new TextButton("Create User/Login", buttonSkin, "default");
+        settingsButton = new ImageButton(buttonSkin, "settings");
+
+        setButtonLayout();
 
 
         // Add click listeners to buttons
@@ -45,7 +61,12 @@ public class MainMenuScreen implements Screen {
                 game.setScreen(new SettingsScreen(game));
             }
         });
-
+        loginButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new LeaderboardScreen(game));
+            }
+        });
         leaderboardButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -57,24 +78,32 @@ public class MainMenuScreen implements Screen {
         stage.addActor(multiplayerButton);
         stage.addActor(settingsButton);
         stage.addActor(leaderboardButton);
-
-        // Set button positions
-        multiplayerButton.setPosition(100, 300);
-        settingsButton.setPosition(100, 200);
-        leaderboardButton.setPosition(100,100);
+        stage.addActor(loginButton);
 
         // Set input processor
         Gdx.input.setInputProcessor(stage);
+    }
+
+    private void setButtonLayout() {
+        float buttonWidth = 200;
+        float buttonHeight = 50;
+        float centerX = (Gdx.graphics.getWidth() - buttonWidth) / 2;
+
+        multiplayerButton.setBounds(centerX, 300, buttonWidth, buttonHeight);
+        leaderboardButton.setBounds(centerX, 200, buttonWidth, buttonHeight);
+        loginButton.setBounds(centerX, 100, buttonWidth, buttonHeight);
+        settingsButton.setBounds(Gdx.graphics.getWidth() - settingsButton.getWidth() - 10, Gdx.graphics.getHeight() - settingsButton.getHeight() - 10, 50, 50);
     }
 
     @Override
     public void render(float delta) {
         // Clear the screen
         Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // Draw background
         batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.end();
 
         // Draw stage
@@ -107,6 +136,7 @@ public class MainMenuScreen implements Screen {
     public void dispose() {
         // Dispose of any resources held by this screen
         stage.dispose();
+        buttonSkin.dispose();
     }
 
     // Other methods from the Screen interface
--- desktop/src/com/mygdx/tank/DesktopLauncher.java
+++ desktop/src/com/mygdx/tank/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Tank Maze Mayhem");
+		config.setWindowedMode(800, 480);
 		new Lwjgl3Application(new TankMazeMayhem(new DesktopAPI()), config);
 	}
 }
