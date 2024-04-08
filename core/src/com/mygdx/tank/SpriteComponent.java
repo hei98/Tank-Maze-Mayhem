912371001a461649ee5584c0a94d23572f1b2a40
andretan <andretan@stud.ntnu.no>
2024-04-08 16:30:06 +0200

    updated origin coordinates of spriteComponent to be more acccurate towards center

--- core/src/com/mygdx/tank/SpriteComponent.java
+++ core/src/com/mygdx/tank/SpriteComponent.java
@@ -8,7 +8,7 @@ public class SpriteComponent implements Component {
 
     public SpriteComponent(String path) {
         this.sprite = new Sprite(new Texture(path));
-        this.sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
+        this.sprite.setOrigin(sprite.getWidth() / 2, (sprite.getHeight() - sprite.getHeight() / 3) / 2);
     }
 
     public Sprite getSprite() {
